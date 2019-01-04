bf6f38a69b77a2bcbc2738e79022ce332d859c48
Dennis Johansson <dennis@dnjo.net>
2019-01-04 20:55:15 +0100

    Add update document loading spinner

--- src/components/UpdateDocument.js
+++ src/components/UpdateDocument.js
@@ -4,13 +4,29 @@ import { API } from 'aws-amplify';
 import { Button, Col, Container, Input, Modal, ModalBody, ModalFooter, ModalHeader, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 
+const LoadingSpinner = (props) => {
+  return (
+    <>
+      <div className={props.loading ? 'd-flex justify-content-center h-100' : 'd-none'}>
+        <div className="d-inline-flex align-self-center">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      </div>
+      <div style={ props.loading ? { visibility: 'hidden' } : {} }>{props.children}</div>
+    </>
+  )
+};
+
 class UpdateDocument extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       ocrText: '',
-      displayDeleteModal: false
+      displayDeleteModal: false,
+      loading: true
     };
 
     this.getDocumentDataPromise = this.getDocumentDataPromise.bind(this);
@@ -24,7 +40,7 @@ class UpdateDocument extends Component {
   getDocumentDataPromise() {
     return new Promise((resolve, reject) => {
       API.get('Backend', `/images/${this.props.match.params.id}`, {}).then(response => {
-        this.setState({ ocrText: response.ocrText });
+        this.setState({ ocrText: response.ocrText, loading: false });
         resolve(response);
       }).catch(error => {
         reject(error);
@@ -65,33 +81,35 @@ class UpdateDocument extends Component {
   render() {
     return (
       <>
-        <Modal isOpen={this.state.displayDeleteModal} centered={true}>
-          <ModalHeader>Confirm document delete</ModalHeader>
-          <ModalBody>Are you sure that you want to delete the document?</ModalBody>
-          <ModalFooter>
-            <Button onClick={this.deleteDocument} color="primary">Delete</Button>
-            <Button onClick={this.toggleDeleteModal} color="secondary">Cancel</Button>
-          </ModalFooter>
-        </Modal>
-        <Container>
-          <Row className="mb-3">
-            <Col className="p-0">
-              <div className="float-right">
-                <Link to={`${this.props.match.url}/source`}>
-                  <Button>View source document</Button>
-                </Link>
-                <Button onClick={this.toggleDeleteModal} className="ml-3" color="danger">Delete document</Button>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-        <Document documentUrl={this.props.match.url}
-                  mountDataPromise={this.getDocumentDataPromise}
-                  submitPromise={this.updateDocumentPromise}
-                  submitButtonName="Update"
-                  successMessage="Document saved."
-                  errorMessage="Failed to save document."
-                  formElements={[this.updateOcrTextElement()]} />
+        <LoadingSpinner loading={this.state.loading}>
+          <Modal isOpen={this.state.displayDeleteModal} centered={true}>
+            <ModalHeader>Confirm document delete</ModalHeader>
+            <ModalBody>Are you sure that you want to delete the document?</ModalBody>
+            <ModalFooter>
+              <Button onClick={this.deleteDocument} color="primary">Delete</Button>
+              <Button onClick={this.toggleDeleteModal} color="secondary">Cancel</Button>
+            </ModalFooter>
+          </Modal>
+          <Container>
+            <Row className="mb-3">
+              <Col className="p-0">
+                <div className="float-right">
+                  <Link to={`${this.props.match.url}/source`}>
+                    <Button>View source document</Button>
+                  </Link>
+                  <Button onClick={this.toggleDeleteModal} className="ml-3" color="danger">Delete document</Button>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+          <Document documentUrl={this.props.match.url}
+                    mountDataPromise={this.getDocumentDataPromise}
+                    submitPromise={this.updateDocumentPromise}
+                    submitButtonName="Update"
+                    successMessage="Document saved."
+                    errorMessage="Failed to save document."
+                    formElements={[this.updateOcrTextElement()]} />
+        </LoadingSpinner>
       </>
     )
   }
